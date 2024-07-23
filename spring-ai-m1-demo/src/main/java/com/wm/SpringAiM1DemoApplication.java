b2833141fb63473b094d7961f7fdc3c2a33e65f2
wangmhww <wangmhww@163.com>
2024-07-23 17:18:58 +0800

    初始化

---
+++ spring-ai-m1-demo/src/main/java/com/wm/SpringAiM1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.wm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiM1DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiM1DemoApplication.class, args);
+    }
+
+}
